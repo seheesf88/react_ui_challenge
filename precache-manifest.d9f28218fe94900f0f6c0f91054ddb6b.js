dd7ef797007f98436d88cc875e4674ecdc9816f1
SeHee Son <seheeson@MacBook-Pro.local>
2020-08-31 01:06:57 -0700

    Updates

---
+++ precache-manifest.d9f28218fe94900f0f6c0f91054ddb6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5392eaa0d8bfbcc6d92a0a3e02b99898",
+    "url": "/react_ui_challenge/index.html"
+  },
+  {
+    "revision": "03f1f0fac12f921f4428",
+    "url": "/react_ui_challenge/static/css/main.bd435ffc.chunk.css"
+  },
+  {
+    "revision": "6d272160de66d02e3500",
+    "url": "/react_ui_challenge/static/js/2.8e271150.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_ui_challenge/static/js/2.8e271150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03f1f0fac12f921f4428",
+    "url": "/react_ui_challenge/static/js/main.b354f8f1.chunk.js"
+  },
+  {
+    "revision": "1371613248edd880459e",
+    "url": "/react_ui_challenge/static/js/runtime-main.aca6a594.js"
+  }
+]);
