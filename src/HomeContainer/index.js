b178e4a5c74dd280bee7e0b33e55faac7f3fa66f
SeHee Son <seheeson@MacBook-Pro.local>
2020-08-31 01:26:58 -0700

    testing

--- src/HomeContainer/index.js
+++ src/HomeContainer/index.js
@@ -11,7 +11,7 @@ class HomeContainer extends Component {
     return(
       <div className="home">
         <div className="home_box">
-          <div className="title_home">My Story</div>
+          <div className="title_home">My Storym</div>
           <div className="book_btn" onClick={this.startBook}></div>
           <img className="ourbook" src={'./ourbook.jpg'} />
         </div>
